e7e2fca571a80f69974f7df9729dabcc652953dc
elis1386 <elis1386@gmail.com>
2023-02-17 09:54:26 +0200

    add details for users

--- src/app/components/admin_/components/contacts-details/contacts-details.component.ts
+++ src/app/components/admin_/components/contacts-details/contacts-details.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AdminService } from '../../services/admin.service';
+import { User } from '../../user';
 
 @Component({
   selector: 'app-contacts-details',
   templateUrl: './contacts-details.component.html',
-  styleUrls: ['./contacts-details.component.css']
+  styleUrls: ['./contacts-details.component.css'],
 })
-export class ContactsDetailsComponent {
+export class ContactsDetailsComponent implements OnInit {
+  user!: Observable<User>;
+  id!: number;
 
+  constructor(
+    private activateRouter: ActivatedRoute,
+    private adminService: AdminService
+  ) {}
 
+  ngOnInit(): void {
+    this.activateRouter.params.subscribe((params) => this.id = params?.['id'])
+    this.user = this.adminService.getPerson(this.id)
+  }
 }
